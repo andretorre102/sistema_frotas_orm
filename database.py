ca619bd754936b69134664e39e9f4a74988e908a
andretorre102 <andretorre@windowslive.com>
2026-03-06 15:09:32 -0300

    Remoção da importação da declarative_base, uma vez que já foi importada no arquivo models.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker #declarative_base já foi importada no arquivo models.py
 
 load_dotenv() # Carregador de variáveis de ambiente do arquivo .env
 
